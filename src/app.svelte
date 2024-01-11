03caa11e935542fbad8b6e61167d1fa31f6e87ab
PokeFred <Cedric.paelmke@gmx.de>
2024-01-11 18:29:43 +0100

    add FontAwesome

--- src/app.svelte
+++ src/app.svelte
@@ -2,4 +2,9 @@
     <title>My Website</title>
 </svelte:head>
 
-<div class="text-3xl">My Website</div>
+<script lang="ts">
+    import Icon from "svelte-awesome"
+    import {faSmile} from "@fortawesome/free-solid-svg-icons/faSmile"
+</script>
+
+<div class="text-3xl">My Website <Icon data={faSmile} /></div>
